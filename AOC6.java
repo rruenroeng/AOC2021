cedf344939d5d9af63826712730af495985b4474
Ryan Ruenroeng <rruenroeng@gmail.com>
2021-12-21 09:00:48 -0500

    We finished day 6! Ow ow!

--- AOC6.java
+++ AOC6.java
@@ -4,18 +4,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+//Round 1
+//365131 is right!
+
+//Round 2
+//1041836936 is too low
+
 class AOC6{
     public static void main(String[] args) throws IOException {
+        int NUMBER_OF_DAYS = 256;
         //Setup Code
-        int[][] grid = new int[1000][1000];
-        int totalCount = 0;
+        long[] arrayOfAges = new long[9];
+        long totalCount = 0;
 
-        for(int i=0;i<grid.length;i++)
-            for(int j=0;j<grid[0].length;j++) {
-                grid[i][j]=0;
-            }
-        System.out.println("Successfully instantiated a grid of zeros");
-        File file = new File("thermalInput.txt");
+        File file = new File("AOC6Lanternfish.txt");
         BufferedReader br;
         try {
             br = new BufferedReader(new FileReader(file));
@@ -23,78 +25,34 @@ class AOC6{
             e1.printStackTrace();
             return;
         }
-        String input = "Initialized";
+        String input;
 
-        while(input!=null){
-            try {
-                input = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-                br.close();
-                return;
-            }
-            if(input==null) break;
-            input = input.replace(" -> ", ",");
-            String [] splitInput = input.split(",");
-            int x1 = Integer.parseInt(splitInput[0]);
-            int y1 = Integer.parseInt(splitInput[1]);
-            int x2 = Integer.parseInt(splitInput[2]);
-            int y2 = Integer.parseInt(splitInput[3]);
-            //Handle Diagonals
-
-            if(x2!=x1&&y1!=y2) {
-                int xSource = Math.min(x1, x2);
-                int xTarget = Math.max(x1, x2);
-                int ySource,yTarget;
-                if(xSource == x1) {
-                    ySource = y1;
-                    yTarget = y2;
-                }
-                else {
-                    ySource = y2;
-                    yTarget = y1;
-                }
-                System.out.println(ySource);
-                for(int i = 0; i <= xTarget-xSource; i++) {
-                    if(yTarget>ySource) {
-                        grid[xSource+i][ySource+i]++;
-                    }
-                    else {
-                        grid[xSource+i][ySource-i]++;
-                    }
-                }
-                System.out.println("I did a diagonal!");
-                continue;    
-            }
-            //Handle Horizontal Lines
-            if(x2!=x1) {
-                // System.out.println("X1: " +x1+" X2: " +x2);
-                int y = y1;
-                int xSource = Math.min(x1, x2);
-                int xTarget = Math.max(x1, x2);
-                for(int i = xSource; i <= xTarget; i++) {
-                    grid[i][y]++;
-                }
-                continue;    
-            }
-            if(y2!=y1) {
-                int x = x1;
-                int ySource = Math.min(y1, y2);
-                int yTarget = Math.max(y1, y2);
-                for(int i = ySource; i <= yTarget; i++) {
-                    grid[x][i]++;
-                }
-                continue;    
-            }
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            br.close();
+            return;
         }
-        for(int i=0;i<grid.length;i++){
-            System.out.println();
-            for(int j=0;j<grid[0].length;j++) {
-                System.out.print(" " + grid[j][i]);
-                if(grid[i][j]>1) {
-                    totalCount++;
-                }
+        String[] fish = input.split(",");
+        
+        //Take a snapshot of all fish of all ages at t=0
+        for(int i=0; i< fish.length; i++) {
+            arrayOfAges[Integer.parseInt(fish[i])]++;
+        }
+
+        //Let time pass
+        for(int i = 0; i<NUMBER_OF_DAYS; i++) {
+            long temp = arrayOfAges[0];
+            for(int j = 0; j < (arrayOfAges.length-1); j++) {
+                arrayOfAges[j] = arrayOfAges[j+1];
             }
+            arrayOfAges[6]+=temp;
+            arrayOfAges[8]=temp;
+        }
+
+        for(int i=0;i<arrayOfAges.length;i++){
+            totalCount+=arrayOfAges[i];    
         }
         System.out.println("Total Count: " + totalCount);
         br.close();
